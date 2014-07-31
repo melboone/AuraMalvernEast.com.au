de86e85690a06d750cb01536383603ce661eff39
Julian Litcanu <email@julian.id.au>
2014-07-31 13:02:33 +1000

    modify readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Aura Malvern East Project
--- includes/menu.php
+++ includes/menu.php
@@ -4,6 +4,6 @@
 <div id="menu">
 <li class="menu2"><a href="index.php" title="Architecture">Architecture</a></li>
 <li class="menu3"><a href="interiors.php" title="Interiors">interior</a></li>
-<li class="menu4"><a href="location.php" title="Location">location</a></li>
 <li class="menu5"><a href="lifestyle.php" title="Lifestyle">lifestyle</a></li>
+<li class="menu4"><a href="location.php" title="Location">location</a></li>
 </div>
